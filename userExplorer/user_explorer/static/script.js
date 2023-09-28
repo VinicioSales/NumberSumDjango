0aea8cccdd27ae904a57b277f46d3b213ee39eb7
VinicioSales <vinicio.santtana@gmail.com>
2023-09-27 22:15:12 -0300

    feat: Add layout

--- userExplorer/user_explorer/static/script.js
+++ userExplorer/user_explorer/static/script.js
@@ -1,15 +1,31 @@
 function sum() {
     const num = document.getElementById("num").value;
-    fetch("/user_explorer/sum/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value
-        },
-        body: `num=${num}`
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("result").innerText = "Result: " + data.result;
-    });
+    res = isNumeric(num)
+    if (res) {
+        console.log(`num: ${num}`);
+        fetch("/user_explorer/sum/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value
+            },
+            body: `num=${num}`
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("title").innerText = "Result: " + data.result;
+        });
+    } else {
+        alert("Numbers only!");
+    }
+    }
+
+function isNumeric(data) {
+    const regex = /^[0-9]+$/;
+
+    if (data.match(regex)) {
+        return true;
+    } else {
+        return false;
+    }
 }
